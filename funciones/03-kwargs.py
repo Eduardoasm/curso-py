0171486b2f4608bc16e789e9ccdc7dbfdd555998
Eduardo Sequeira <eduardoasm19@gmail.com>
2024-11-20 22:32:35 -0400

    functions , kwargs and depurations

---
+++ funciones/03-kwargs.py
@@ -0,0 +1,5 @@
+def get_product(**data):
+    print(data["id"], data["name"])
+
+
+get_product(id="123", name="Charlie")
---
+++ funciones/04-depuracion.py
@@ -0,0 +1,10 @@
+def get_length(string):
+    resultado = 0
+    for _ in string:
+        resultado += 1
+    return resultado
+
+
+print("hola")
+l = get_length("hola")
+print(l)
